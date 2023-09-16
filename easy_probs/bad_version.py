84c39f706e39f64da6d54edc3a27f3f9f7f0b25b
shovarnu <shovarnu@faarms.com>
2023-09-16 22:17:24 +0530

    bad_version finder

---
+++ easy_probs/bad_version.py
@@ -0,0 +1,24 @@
+"""
+You have N versions of a software, we want to
+find that the first "bad" version of the
+software
+
+How to determine a bad version?
+
+We will be given a method called "isBadVersion" 
+by default, it returns a boolean, true if the 
+version you pass to it is bad,
+false otherwise
+
+Important note:
+if a version is bad, all versions after it are bad
+
+Can our input be "0", meaning that we have no versions
+of our software?
+
+No, our input, N, will represent versions starting
+from [1...N] and we'll never have N be 0
+"""
+
+
+
