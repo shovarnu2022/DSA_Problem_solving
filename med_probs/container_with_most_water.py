c7e7d9e43eec6b6de2eb21c6399705fa173107b6
shovarnu <shovid2019@gmail.com>
2023-09-21 12:32:13 +0530

    ///

---
+++ med_probs/container_with_most_water.py
@@ -0,0 +1,10 @@
+"""
+Given n non_negative integers where 
+each integer represent the hegiht 
+of a vertical line on a chart
+
+Find two lines, which together with 
+x-axis
+forms a container, that holds the biggest
+amount of water. return the area of the water.
+"""
