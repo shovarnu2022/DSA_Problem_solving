79b697fd2f3a589c67f7be72508f4c3e89405d37
shovarnu <shovarnudutta@pisquare.io>
2024-02-07 19:29:35 +0530

    vemkve

--- python_problems/common_letters_bw_two_strings.py
+++ python_problems/common_letters_bw_two_strings.py
@@ -5,3 +5,14 @@ REENE
 N IS COMMON BETWEEN TWO STRINGS
 """
 
+def common_letters():
+    str1 = input("Enter first string: ")
+    str2 = input("Enter second string: ")
+    s1 = set(str1)
+    s2 = set(str2)
+    print(s1)
+    print(s2)
+    lst=s1&s2
+    print(lst)
+
+common_letters()
