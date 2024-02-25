dff5fa7eb6eb21cf8c649f37a728edda392c1836
shovarnu2022 <shovid2019@gmail.com>
2024-02-25 23:45:58 +0530

    Valid mountain array problem statement

--- python_problems/valid_mountain_array.py
+++ python_problems/valid_mountain_array.py
@@ -38,7 +38,72 @@ How would we check for an increasing subsequence?
 
 > We can do it in a loop and at each iteration, we check to
 see if the current element is bigger than the previous element
+
+> If the above condition fails, we break from the loop
+
+What do we need to keep track of?
+
+> Where the increasing subsequence ends
+
+> Why do we need to keep track of it?
+
+To know there to start checking for a decreased sequence
+
+> How do we keep track of it?
+
+the pointer used to loop over our array while we're checking
+for the increasing subsequence needs to be saved
+
+> When would we return false before even checking for a
+decreasing subsequence?
+
+if we didn't move from our location (meaning that there
+wasn't ANY increasing subsequence)
+
+if we already reached the end of the array
+
+**Implementation**
+
+Steps
+
+> Loop over array starting from loop index as long as it's
+increasing
+
+i=1
+while i< len(A) and A[i] > A[i-1]:
+    i+=1
+if i == 1 or i == len(A):
+   return False
+while i < len(A) and A[i] < A[i-1]:
+   i+=1
+return i == len(A)
+
+
 """
+
+
+class Solution:
+    def validMountainArray(self, A: list[int]) -> bool:
+        i = 1
+        while i < len(A) and A[i] > A[i-1]:
+            i = i + 1
+        if i == 1 or i == len(A):
+            return False
+
+        while i < len(A) and A[i] < A[i-1]:
+            i += 1
+
+        return i == len(A)
+
+
+
+
+
+
+
+
+
+
 
 
 
